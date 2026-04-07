b7c38612b246504fe6f3d576263cab9d478ca9ec
Aleslong <yevbes@gmail.com>
2026-04-07 16:28:50 +0200

    add User entity, change app prop for DB connection

--- src/main/java/com/yevbes/marketplace/entity/User.java
+++ src/main/java/com/yevbes/marketplace/entity/User.java
@@ -1,4 +1,34 @@
 package com.yevbes.marketplace.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
